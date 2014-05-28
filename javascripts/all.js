52c235789ea624428ed65623aa5eef26f1b7b685
kdjlaw <mjesuele@gmail.com>
2014-05-28 10:23:30 -0700

    Fixed some validation errors cause I'm good like that

--- javascripts/all.js
+++ javascripts/all.js
@@ -9180,8 +9180,7 @@ $(document).foundation();
 (function() {
   $(function() {
     return $('#contact-form').on('valid', function(e) {
-      var failureHandler, field, formData, successHandler, _i, _len, _ref,
-        _this = this;
+      var failureHandler, field, formData, successHandler, _i, _len, _ref;
       e.preventDefault();
       formData = {};
       _ref = $(this).serializeArray();
@@ -9189,35 +9188,31 @@ $(document).foundation();
         field = _ref[_i];
         formData[field['name']] = field['value'];
       }
-      successHandler = function(response) {
-        var successAlert;
-        window.response = response;
-        console.log(response);
-        if (response === 200) {
-          successAlert = $('\
-					<div data-alert class="alert-box success radius">\
-					  Message sent! We will be in touch with you shortly.\
-					  <a href="#" class="close">&times;</a>\
-					</div>');
-          $(_this).append(successAlert).foundation();
-          $(_this).unbind('submit');
-          return $(_this).submit(function() {
-            return false;
-          });
-        } else {
-          return failureHandler();
-        }
-      };
-      failureHandler = function(response) {
-        var failureAlert;
-        console.log(response);
-        failureAlert = $('\
-				<div data-alert class="alert-box alert radius">\
-					  Something went wrong. Please try again.\
-					  <a href="#" class="close">&times;</a>\
-				</div>');
-        return $(_this).append(failureAlert).foundation();
-      };
+      successHandler = (function(_this) {
+        return function(response) {
+          var successAlert;
+          window.response = response;
+          console.log(response);
+          if (response === 200) {
+            successAlert = $('<div data-alert class="alert-box success radius"> Message sent! We will be in touch with you shortly. <a href="#" class="close">&times;</a> </div>');
+            $(_this).append(successAlert).foundation();
+            $(_this).unbind('submit');
+            return $(_this).submit(function() {
+              return false;
+            });
+          } else {
+            return failureHandler();
+          }
+        };
+      })(this);
+      failureHandler = (function(_this) {
+        return function(response) {
+          var failureAlert;
+          console.log(response);
+          failureAlert = $('<div data-alert class="alert-box alert radius"> Something went wrong. Please try again. <a href="#" class="close">&times;</a> </div>');
+          return $(_this).append(failureAlert).foundation();
+        };
+      })(this);
       $.ajax({
         method: 'POST',
         data: formData,
--- javascripts/contactForm.js
+++ javascripts/contactForm.js
@@ -1,8 +1,7 @@
 (function() {
   $(function() {
     return $('#contact-form').on('valid', function(e) {
-      var failureHandler, field, formData, successHandler, _i, _len, _ref,
-        _this = this;
+      var failureHandler, field, formData, successHandler, _i, _len, _ref;
       e.preventDefault();
       formData = {};
       _ref = $(this).serializeArray();
@@ -10,35 +9,31 @@
         field = _ref[_i];
         formData[field['name']] = field['value'];
       }
-      successHandler = function(response) {
-        var successAlert;
-        window.response = response;
-        console.log(response);
-        if (response === 200) {
-          successAlert = $('\
-					<div data-alert class="alert-box success radius">\
-					  Message sent! We will be in touch with you shortly.\
-					  <a href="#" class="close">&times;</a>\
-					</div>');
-          $(_this).append(successAlert).foundation();
-          $(_this).unbind('submit');
-          return $(_this).submit(function() {
-            return false;
-          });
-        } else {
-          return failureHandler();
-        }
-      };
-      failureHandler = function(response) {
-        var failureAlert;
-        console.log(response);
-        failureAlert = $('\
-				<div data-alert class="alert-box alert radius">\
-					  Something went wrong. Please try again.\
-					  <a href="#" class="close">&times;</a>\
-				</div>');
-        return $(_this).append(failureAlert).foundation();
-      };
+      successHandler = (function(_this) {
+        return function(response) {
+          var successAlert;
+          window.response = response;
+          console.log(response);
+          if (response === 200) {
+            successAlert = $('<div data-alert class="alert-box success radius"> Message sent! We will be in touch with you shortly. <a href="#" class="close">&times;</a> </div>');
+            $(_this).append(successAlert).foundation();
+            $(_this).unbind('submit');
+            return $(_this).submit(function() {
+              return false;
+            });
+          } else {
+            return failureHandler();
+          }
+        };
+      })(this);
+      failureHandler = (function(_this) {
+        return function(response) {
+          var failureAlert;
+          console.log(response);
+          failureAlert = $('<div data-alert class="alert-box alert radius"> Something went wrong. Please try again. <a href="#" class="close">&times;</a> </div>');
+          return $(_this).append(failureAlert).foundation();
+        };
+      })(this);
       $.ajax({
         method: 'POST',
         data: formData,
